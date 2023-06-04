c15970e2ae2ec70096e55ff2aed4c918085a11f0
CharlieScarver <s.apostolovd@gmail.com>
2023-06-05 00:05:52 +0300

    Test and example classes - might delete later

---
+++ ListVisualizer/ViewModel/NewViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListVisualizer.ViewModel
+{
+    public class NewViewModel : ListViewModel
+    {
+        protected override void ToggleCheckbox(object itemColumn)
+        {
+            //base.ToggleCheckbox(itemColumn);
+            Console.WriteLine("Do nothing instead");
+        }
+    }
+}
